f173774ba368b328326ba420040723748c8b8aae
Blake Loring <blake_l@parsed.uk>
2015-12-18 20:20:34 +0000

    Beginning to calculate if summarization can be done

--- src/analysis/summarize.cpp
+++ src/analysis/summarize.cpp
@@ -1,13 +1,27 @@
 #include "summarize.h"
+#include "utils.h"
 
 using namespace JIT;
 using namespace JIT::Expressions;
 
 bool Summarize::canSummarize(SafeExpression expression) const {
-	return false;
+	
+	if (expression->getExpressionType() != ExpressionType::IfType) {
+		return false;
+	}
+
+	if (!AnalysisUtils::isAtomic(expression->getArguments()[2])) {
+		return false;
+	}
+
+	return true;
 }
 
 JIT::Expressions::SafeExpression Summarize::doAnalysis(JIT::Expressions::SafeExpression expression) const {
-	printf("Summarized\n");
+	
+	if (canSummarize(expression)) {
+		printf("I might be able to do something here\n");
+	}
+
 	return expression;
 }
